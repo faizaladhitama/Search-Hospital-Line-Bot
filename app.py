cba64d5768e2fe3ab481b89db7caa1314db169c5
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2018-04-21 11:05:29 +0700

    Add building word to banned word

--- app.py
+++ app.py
@@ -50,23 +50,17 @@ def callback():
 
     return 'OK'
 
-#User send location
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
-    #source_type = event.source.type
-    #state = db[event.source.userId]    
-    
+
     location_obj = event.message
     longitude = location_obj.longitude
     latitude = location_obj.latitude
     address = location_obj.address     
  
     src = event.source
-    json_data = jsonpickle.encode(src)    
-    print(json_data)    
+    json_data = jsonpickle.encode(src)
     data = json.loads(json_data)
-    print(data)    
-    print(data['user_id'])
     
     try:
         db[str(data['user_id'])] = "{} , {}".format(latitude,longitude)
@@ -74,9 +68,9 @@ def handle_location_message(event):
         db[str(data['user_id'])] = "{} , {}".format(latitude,longitude)
 
     reply = "Address : {}\nLongitude : {}\nLatitude : {}\nDatabase : {}".format(address,longitude,latitude,db[str(data['user_id'])])
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply)) #reply the same message from user
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply))
 
-#User send text
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text #message from user
@@ -87,14 +81,11 @@ def handle_text_message(event):
             event.reply_token,
             TextSendMessage(text=reply)) #reply the same message from user
     elif text == "/hospital":
-        #try:
+        try:
             reply = "Pengurutan Rumah Sakit dengan jarak terdekat :\n"
             src = event.source
-            json_data = jsonpickle.encode(src)    
-            print(json_data)    
+            json_data = jsonpickle.encode(src)
             data = json.loads(json_data)
-            print(data)    
-            print(data['user_id'])
             
             LOCATION = db[str(data['user_id'])]
             API_KEY = "AIzaSyDrnC_K1xWhzPFbTe8RXPQgerRTfs2cftg"
@@ -104,7 +95,7 @@ def handle_text_message(event):
             LINK = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
 
             newResult = []
-            banned_word = ['pkbi', 'akademi', 'instalasi', 'gedung', 'klinik', 'bedah', 'penyakit', 'clinichek', 'spesialis', 'poli', 'dms', 'sehat', 'apotik', 'anasthesi', 'check', 'terapi', 'jenazah', 'simulasi', 'respirasi', 'kantin', 'mesjid', 'direksi', 'bc', 'uber', 'clinic', 'health', 'care']
+            banned_word = ['pkbi', 'akademi', 'instalasi', 'gedung', 'klinik', 'bedah', 'penyakit', 'clinichek', 'spesialis', 'poli', 'dms', 'sehat', 'apotik', 'anasthesi', 'building', 'check', 'terapi', 'jenazah', 'simulasi', 'respirasi', 'kantin', 'mesjid', 'direksi', 'bc', 'uber', 'clinic', 'health', 'care']
             banned_sentence = ['rumah sakit', 'rs', 'kusuma putra']
             
             RADIUS = int(RADIUS_DEFAULT)
@@ -124,12 +115,11 @@ def handle_text_message(event):
                 if clear and name.lower() not in banned_sentence and name not in newResult:
                     newResult.append(name)
             for result in newResult:
-                reply += result + "\n"
-            print(reply)
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply),timeout=10) #reply the same message from user
-        #except:            
-        #    reply = "Please send your location first"
-        #    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply)) #reply the same message from user
+                reply += "- " + result + "\n"
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply),timeout=10)
+        except:            
+            reply = "Please send your location first"
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply))
     
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))

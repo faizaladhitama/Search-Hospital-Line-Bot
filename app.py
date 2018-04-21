a7abce271474a116cd3d9368bc267b395ed30476
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2018-04-21 10:27:46 +0700

    change port

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def handle_text_message(event):
 
 import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    app.run(host='0.0.0.0',port=int(os.environ['PORT'])
